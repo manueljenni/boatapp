0dde85f69a7fc2591299ea5b6d8ef297682c9445
manueljenni <manuelnoahjenni@outlook.com>
2023-05-19 02:55:11 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  viewport: "width=device-width, initial-scale=1",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex jsutify-center items-center min-h-screen text-center">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
